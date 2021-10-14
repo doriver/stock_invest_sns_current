b31f94a374c0e6d7e5f4801ec3872d427931ad72
웹개발 <tkdduq241@naver.com>
2021-10-14 11:54:46 +0900

    10-14 다른컴, note쪽만

--- src/main/java/com/sns/invest/ex/note1.java
+++ src/main/java/com/sns/invest/ex/note1.java
@@ -1,5 +1,7 @@
 package com.sns.invest.ex;
 
+import com.sns.invest.user.model.User;
+
 public class note1 {
 
 }
@@ -7,11 +9,16 @@ public class note1 {
 //	* User userInformation(int userId) 
 //	selectUserByUserId , user테이블에서 입력받은 id에 해당하는 모든 정보를 가져옴 
 //
+//	* User signIn(String idForLogin, String passwordForLogin)
+//	selectUserByIdPassword, user테이블에서 입력받은 로그인id, 패스워드에 해당하는 모든 정보를 가져옴
+	
+	
+	
 //			UserRestController에서
 //	* @PostMapping("/sign_in") Map<String, String> signIn
-//	( @RequestParam("idForLogin") String idForLogin
+//	(@RequestParam("idForLogin") String idForLogin
 //	, @RequestParam("passwordForLogin") String passwordForLogin
-//	, HttpServletRequest request )
+//	, HttpServletRequest request)
 //	User user = userBO.signIn(idForLogin, passwordForLogin);
 //	HttpSession session = request.getSession();
 //	session.setAttribute("userId", user.getId());
