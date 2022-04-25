ede0fdd21a1056bac317869f6477d626a624c192
doriver <tkdduq241@naver.com>
2022-04-25 16:10:57 +0900

    ..

--- src/main/java/com/sns/invest/common/FileManagerService.java
+++ src/main/java/com/sns/invest/common/FileManagerService.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
-
+// a
 public class FileManagerService {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
