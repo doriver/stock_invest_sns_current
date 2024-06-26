b9d69567774fb77aefcabbedfb16ff6178af2c07
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-06-26 16:14:28 +0900

    redis로 좋아요 기능 구현 시작

--- src/main/java/com/sns/invest/post/bo/LikeBO.java
+++ src/main/java/com/sns/invest/post/bo/LikeBO.java
@@ -12,7 +12,7 @@ public class LikeBO {
 	
 	//사용자기준 좋아요 여부 체크 , 좋아요 되있으면 true, 안되있으면 false
 	public boolean existLike(int postId, int userId, String type) {
-		int count = likeDAO.selectCountLike(postId, userId, type);
+		int count = likeDAO.selectCountLike(postId, userId, type); // 해당글 좋아요에 사용자id 있는지 여부
 		
 		if(count >= 1) {
 			return true;
@@ -26,21 +26,21 @@ public class LikeBO {
 		
 		// 만약 해당 포스트에 좋아요가 되어 있다면 좋아요 취소하고 , false
 		if(this.existLike(postId, userId, type)) {
-			likeDAO.deleteLike(postId, userId, type);
+			likeDAO.deleteLike(postId, userId, type); // 해당글 좋아요에서 사용자id 삭제
 			return false;
 		} else  {  // 만약 해당 포스트에 좋아요가 안되어 있다면 좋아요되고 , true
-			likeDAO.insertLike(postId, userId, type);
+			likeDAO.insertLike(postId, userId, type); // 해당글 좋아요에 사용자id 추가
 			return true;
 		}
 	}
 	
 	// 좋아요 갯수 
 	public int countLike(int postId, String type) {
-		return likeDAO.selectCountLikeByPostIdType(postId, type);
+		return likeDAO.selectCountLikeByPostIdType(postId, type); // 해당글 좋아요 원소개수 얻음
 	}
 	
 	public int deleteLikeInPost(int postId, String type) {
-		return likeDAO.deleteLikeInPost(postId, type);
+		return likeDAO.deleteLikeInPost(postId, type); // 해당글에 대응하는 key 삭제
 	}
 
 	
