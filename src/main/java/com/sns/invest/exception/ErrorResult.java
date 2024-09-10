79070f279501d2428c9c1bb144fbab9f835e0bd7
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-09-10 17:08:35 +0900

    서버쪽 API예외처리 구현

--- src/main/java/com/sns/invest/exception/ErrorResult.java
+++ src/main/java/com/sns/invest/exception/ErrorResult.java
@@ -6,6 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class ErrorResult {
-	private String code;
-	private String message;
+	private String code; // 클라이언트에서 이걸로 사용자에게 보여줄 메시지 작성할수 있음
+	private String message; // getMessage()값 , 클라이언트에 넘겨주면 errorLog 찾기 쉬움
 }
--- src/main/java/com/sns/invest/exception/advice/ExControllerAdvice.java
+++ src/main/java/com/sns/invest/exception/advice/ExControllerAdvice.java
@@ -1,10 +1,15 @@
 package com.sns.invest.exception.advice;
 
-import org.springframework.http.HttpStatus;
+import java.net.BindException;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sns.invest.exception.ErrorResult;
 
@@ -15,9 +20,48 @@ import lombok.extern.slf4j.Slf4j;
 public class ExControllerAdvice {
 
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	@ExceptionHandler
-	public ErrorResult exHandler(Exception e) {
+	@ExceptionHandler(Exception.class)
+	public Object exHandler(Exception e, HttpServletRequest request) {
 		log.error("[exceptionHandler] ex", e);
-		return new ErrorResult("Exception", "서버에서 예외 발생");
+		
+		String acceptHeader = request.getHeader("Accept");
+		String message = e.getMessage();
+		if (message == null) {
+			message = "서버 내부 오류"; 
+		}
+		
+		// Accept: text/html 요청에 대해 HTML 응답
+        if (acceptHeader != null && acceptHeader.contains("text/html")) {
+            ModelAndView mav = new ModelAndView("error/errorPage");
+            mav.addObject("message", message);
+            return mav;
+        }
+        
+        // 그 외의 경우 JSON 응답
+		return new ErrorResult("ex", message);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BindException.class)
+	public ErrorResult bindExHandler(BindException e) {
+		log.error("[exceptionHandler] ex", e);
+
+		String message = e.getMessage();
+		if (message == null) {
+			message = "입력(요청)값 오류"; 
+		}
+		return new ErrorResult("bind", message);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ErrorResult mArgExHandler(MethodArgumentNotValidException e) {
+		log.error("[exceptionHandler] ex", e);
+
+		String message = e.getMessage();
+		if (message == null) {
+			message = "입력값 오류"; 
+		}
+		return new ErrorResult("mArg", message);
 	}
 }
